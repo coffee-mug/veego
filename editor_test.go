f57a974cfa70061146f19ef92848a045c374c5d6
Lucas Kostka <lucas.kostka@bsb-education.com>
2019-09-22 18:19:29 +0200

    Train commiting again

--- editor_test.go
+++ editor_test.go
@@ -20,20 +20,24 @@ func (s mockStdin) Write(p []byte) (n int, err error) {
 	for _, b := range p {
 		s.buffer = append(s.buffer, b)
 	}
+
+	return len(p), nil
 }
 
 func TestEditor(t *testing.T) {
 	t.Run("Typing a character displays it on screen", func(t *testing.T) {
 		var editor Editor
-		character := 'a'
+		character := "a"
 
 		stdin := mockStdin{[]byte{}}
 
 		editor.ListenForInputs(stdin)
 
+		stdin.Write([]byte("a"))
+
 		got := editor.buffer
 
-		if got[0] != character {
+		if string(got) != character {
 			t.Errorf("Got %v Want %v", got, character)
 		}
 	})
